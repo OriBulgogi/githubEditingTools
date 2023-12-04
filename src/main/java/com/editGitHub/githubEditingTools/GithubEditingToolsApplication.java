e2b58cffebb0d416a1b2f553b7fb3f6227d015b3
oribulgogi <hn01184@gmail.com>
2023-12-04 13:57:01 +0900

    Initial commit

---
+++ src/main/java/com/editGitHub/githubEditingTools/GithubEditingToolsApplication.java
@@ -0,0 +1,13 @@
+package com.editGitHub.githubEditingTools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubEditingToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubEditingToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/editGitHub/githubEditingTools/GithubEditingToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.editGitHub.githubEditingTools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubEditingToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
